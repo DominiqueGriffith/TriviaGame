d2a147273ca7f32df1799cee051e525398ad41cc
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-05-07 18:44:26 -0400

    Set up questions to dynamically update as an array. The timer broke however.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,4 @@
 var timer = 30;
-
 var intervalId;
 
 window.onload = function run() {
@@ -10,23 +9,195 @@ window.onload = function run() {
 function decrement () {
 
     timer--;
+}
+$("#timer").html("<div>" + timer + "</div>");
 
 
-$("#timer").html("<span>" + timer + "</span>");
+var start = document.getElementById("start");
+var oscarQuiz = document.getElementById("oscarquiz");
+var quizImage = document.getElementById("question");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var choiceE = document.getElementById("E");
+
+var questions = [
+    {question: "Which film won best picture for the year 2000?",
+    imgSrc: "",
+    choiceA: "Gladiator",
+    choiceB: "Crouching Tiger, Hidden Dragon",
+    choiceC: "Erin Brockovich",
+    choiceD: "Traffic",
+    choiceE: "Chocolat",
+
+    },
+    {question: "Which film won best picture for the year 2001?",
+    imgSrc: "",
+    choiceA: "In the Bedroom",
+    choiceB: "A Beautiful Mind",
+    choiceC: "The Lord of the Rings: The Fellowship of the Ring",
+    choiceD: "Moulin Rouge!",
+    choiceE: "Godsford Park",
+    },
+
+    {question: "Which film won best picture for the year 2002?",
+    imgSrc: "",
+    choiceA: "Gangs of New York",
+    choiceB: "Chicago",
+    choiceC: "The Hours",
+    choiceD: "The Lord of the Rings: The Two Towers",
+    choiceE: "",
+    },
+    {question: "Which film won best picture for the year 2003?",
+    imgSrc: "",
+    choiceA: "Lost in Translation",
+    choiceB: "Master and Commander: The Far Side of the World",
+    choiceC: "Mystic River",
+    choiceD: "The Lord of the Rings: The Return of the King!",
+    choiceE: "",
+    },
+    {question: "Which film won best picture for the year 2004?",
+    imgSrc: "",
+    choiceA: "Million Dollar Baby",
+    choiceB: "The Aviator",
+    choiceC: "Finding Neverland",
+    choiceD: "Ray",
+    choiceE: "",
+    },
+    {question: "Which film won best picture for the year 2005?",
+    imgSrc: "",
+    choiceA: "Crash",
+    choiceB: "Brokeback Mountain",
+    choiceC: "Capote",
+    choiceD: "Good Night, and Good Luck",
+    choiceE: "",
+    },
+    {question: "Which film won best picture for the year 2006?",
+    imgSrc: "",
+    choiceA: "Babel",
+    choiceB: "Letters from Iwo Jima",
+    choiceC: "Little Miss Sunshine",
+    choiceD: "The Departed",
+    choiceE: "",
+    },
+    {question: "Which film won best picture for the year 2007?",
+    imgSrc: "",
+    choiceA: "Atonement",
+    choiceB: "No Country for Old Men",
+    choiceC: "Juno",
+    choiceD: "Michael Clayton",
+    choiceE: "",
+    },
+]
+
+var lastQuestionNum = questions.length - 1;
+var questionNum = 0;
+
+// var correct = document.getElementById ("#correct");
 
 
-// $("#correct").on("click", function() {
 
+
+
+
+
+ 
+function renderQuestion(){
+    var q = questions[questionNum];
+    quizImage.innerHTML = "<img src=" + q.imgSrc + ">";
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTMl = q.choiceA;
+    choiceA.innerHTMl = q.choiceB;
+    choiceA.innerHTMl = q.choiceC;
+    choiceA.innerHTMl = q.choiceD;
+    choiceA.innerHTMl = q.choiceE;
+    
 
 }
+
+questionNum = 0;
+renderQuestion()
+
+questionNum++
+renderQuestion()
+
+
+// $(".dynamic").on("click", function() {
+   
+//     if (correct.clicked == true) {
+//         alert("you're correct")
+    //   var  youCorrect = document.getElementsById('correct');
+    //   correct = youCorrect;
+    //     youCorrect[0,1,2,3,4,5].textContent = "You're correct!";
+      
+
+//     }
+
+// });
+
+// }
 
 
 // }
 
-game = document.getElementsByClassName('dynamic');
-game[1].textContent = "Which film won best picture for the year 2001?"
-game[2].textContent = "In the Bedroom"
-game[3].textContent = "A Beautiful Mind"
-game[4].textContent = "The Fellowship of the Ring"
-game[5].textContent = "Moulin Rouge!"
+{/* <h3 class = "dynamic">Which film won best picture for the year 2000?</h3>
+<p id = "correct" class = "dynamic">Gladiator</p>
+<p class = "dynamic">Crouching Tiger, Hidden Dragon</p>
+<p class = "dynamic">Erin Brockovich</p>
+<p class = "dynamic">Traffic</p>
+<p class = "dynamic">Chocolat</p> */}
+
+// gameOne = document.getElementsByClassName('dynamic');
+// gameOne[0].textContent = "Which film won best picture for the year 2001?"
+// gameOne[1].textContent = "In the Bedroom"
+// gameOne[2].textContent = "A Beautiful Mind"
+// gameOne[3].textContent = "The Fellowship of the Ring"
+// gameOne[4].textContent = "Moulin Rouge!"
+// gameOne[5].textContent = "Godsford Park"
+
+// gameTwo = document.getElementsByClassName('dynamic');
+// gameTwo[1].textContent = "Which film won best picture for the year 2002?"
+// gameTwo[2].textContent = "Gangs of New York"
+// gameTwo[3].textContent = "Chicago"
+// gameTwo[4].textContent = "The Hours"
+// gameTwo[5].textContent = "The Lord of the Rings: The Two Towers"
+
+// gameThree = document.getElementsByClassName('dynamic');
+// gameThree[1].textContent = "Which film won best picture for the year 2003?"
+// gameThree[2].textContent = "Lost in Translation"
+// gameThree[3].textContent = "Master and Commander: The Far Side of the World"
+// gameThree[4].textContent = "Mystic River"
+// gameThree[5].textContent = "The Lord of the Rings: The Return of the King!"
+
+// gameFour = document.getElementsByClassName('dynamic');
+// gameFour[1].textContent = "Which film won best picture for the year 2004?"
+// gameFour[2].textContent = "Million Dollar Baby"
+// gameFour[3].textContent = "The Aviator"
+// gameFour[4].textContent = "Finding Neverland"
+// gameFour[5].textContent = "Ray"
+
+// gameFive = document.getElementsByClassName('dynamic');
+// gameFive[1].textContent = "Which film won best picture for the year 2005?"
+// gameFive[2].textContent = "Crash"
+// gameFive[3].textContent = "Brokeback Mountain"
+// gameFive[4].textContent = "Capote"
+// gameFive[5].textContent = "Good Night, and Good Luck"
+
+// gameSix = document.getElementsByClassName('dynamic');
+// gameSix[1].textContent = "Which film won best picture for the year 2006?"
+// gameSix[2].textContent = "Babel"
+// gameSix[3].textContent = "Letters from Iwo Jima"
+// gameSix[4].textContent = "Little Miss Sunshin"
+// gameSix[5].textContent = "The Departed"
+
+// gameSeven = document.getElementsByClassName('dynamic');
+// gameSeven [1].textContent = "Which film won best picture for the year 2007?"
+// gameSeven [2].textContent = "Atonement"
+// gameSeven [3].textContent = "No Country for Old Men"
+// gameSeven [4].textContent = "Juno"
+// gameSeven [5].textContent = "Michael Clayton"
+
+
 run ();
